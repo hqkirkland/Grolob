9311dc12d930fec7fa9b265a88fa1445ccf98315
Hunter Kirkland <hkirkland@vitalinteraction.com>
2018-12-14 11:00:31 -0600

    Made changes in staging on accident. Have to pass them to development.

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ from flask_migrate import Migrate
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 
-from config import mariadb_connection_string, secret_session_key
-
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+from config import mariadb_connection_string, secret_session_key
 
 # Initiate app + settings
 app = Flask(__name__)
